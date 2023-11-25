cd9d9376eaf744c9448332acf9c54dc4c264b9d5
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-11-25 17:52:34 +0530

    Error boundary

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,10 +1,12 @@
 import Localized from "../components/Localized";
 // npm i i18next react-i18next
 
-const AboutPage = () => {
+const AboutPage: React.FC<any> = (props) => {
   return (
     <div>
       <h1>About Page</h1>
+      <h2>Demo of Error Boundary</h2>
+      <p>{props.add()}</p>
       <h2>Demo of i18n and l10n</h2>
       <Localized />
     </div>
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import NetflixPage from "../pages/NetflixPage";
 import UsersPage from "../pages/UsersPage";
@@ -10,32 +10,43 @@ import ContactUsPage from "../pages/ContactUsPage";
 import SpotifyPage from "../pages/SpotifyPage";
 import AddUser from "../components/AddUser";
 import UserDetails from "../components/UserDetails";
-import LoginPage from '../pages/LoginPage';
-import ProtectedRoutes from './ProtectedRoutes';
-import AdminPage from '../pages/AdminPage';
+import LoginPage from "../pages/LoginPage";
+import ProtectedRoutes from "./ProtectedRoutes";
+import AdminPage from "../pages/AdminPage";
+import { ErrorBoundary } from "react-error-boundary";
 
 const MainRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/netflix" element={<NetflixPage />} />
-      <Route path="/users" element={<UsersPage />} />
-      <Route path="/users/add" element={<AddUser />} />
-      <Route path="/users/:id" element={<UserDetails />} />
-      <Route path="/timeline" element={<TimelinePage />} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/auth/login" element={<LoginPage />} />
+    <ErrorBoundary
+      fallback={
+        <div className="alert alert-danger">
+          Something went wrong. Try again later
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/netflix" element={<NetflixPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/users/add" element={<AddUser />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+        <Route path="/timeline" element={<TimelinePage />} />
+        <Route path="/products" element={<ProductsPage />} />
 
-      {/* The following routes are protected. You need to login to access them */}
-      <Route element={<ProtectedRoutes />}>
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/tree-view" element={<TreeViewPage />} />
-        <Route path="/contact-us" element={<ContactUsPage />} />
-        <Route path="/spotify" element={<SpotifyPage />} />
-      </Route>
-    </Routes>
+        <Route path="/about" element={<AboutPage />} />
+
+        <Route path="/auth/login" element={<LoginPage />} />
+
+        {/* The following routes are protected. You need to login to access them */}
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/tree-view" element={<TreeViewPage />} />
+          <Route path="/contact-us" element={<ContactUsPage />} />
+          <Route path="/spotify" element={<SpotifyPage />} />
+        </Route>
+      </Routes>
+    </ErrorBoundary>
   );
-}
+};
 
 export default MainRoutes;
