d90d50b601ac59aa0c058b2da0fb9cb6bfedfa94
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-11-25 23:10:47 +0530

    updated

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,3 +1,4 @@
+import Counter from "../components/Counter";
 import Localized from "../components/Localized";
 // npm i i18next react-i18next
 
@@ -6,9 +7,13 @@ const AboutPage: React.FC<any> = (props) => {
     <div>
       <h1>About Page</h1>
       <h2>Demo of Error Boundary</h2>
-      <p>{props.add()}</p>
+      {/* <p>{props.add()}</p> */}
       <h2>Demo of i18n and l10n</h2>
       <Localized />
+
+      <hr/>
+      <h2>useCallback Hook Demo</h2>
+      <Counter />
     </div>
   )
 }
